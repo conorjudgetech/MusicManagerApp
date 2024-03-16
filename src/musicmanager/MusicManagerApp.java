80f08a885e4f17eb3e6076255b2b8231ce5f30ca
Conor Judge <conorjudgetech@icloud.com>
2024-03-16 19:51:30 +0000

    Initial Format

--- src/musicmanager/MusicManagerApp.java
+++ src/musicmanager/MusicManagerApp.java
@@ -1,20 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package musicmanager;
 
 /**
- *
- * @author work
+ *MusicManagerApp.java
+ * 12/03/24
+ * @author Conor Judge
  */
-public class MusicManagerApp {
 
-    /**
-     * @param args the command line arguments
-     */
+public class MusicManagerApp {
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
