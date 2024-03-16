d3b5368d512bf924cc70dad66c8a72c58aae6f3d
Conor Judge <conorjudgetech@icloud.com>
2024-03-16 20:57:43 +0000

    ArrayListStack

--- src/musicmanager/ArrayListStack.java
+++ src/musicmanager/ArrayListStack.java
@@ -10,4 +10,30 @@ import java.util.ArrayList;
 
 public class ArrayListStack<T> implements Stack<T> {{//<T> the type of elements stored in the stack
     private ArrayList<T> stack;
+    
+    //Constructs an ArrayListStack with an empty ArrayList
+    public ArrayListStack() {
+        stack = new ArrayList<>();
+    }
+    
+    @Override
+    //Pushes a song onto the top of the stack
+    public void push(T item) {
+        stack.add(item);
+    }
+
+    @Override
+    //Removes and returns the song at the top of the stack
+    public T pop() {
+        if (isEmpty()) {
+            return null;
+        }
+        return stack.remove(stack.size() - 1);
+    }
+
+    @Override
+    //Checks if the stack is empty
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
 }
