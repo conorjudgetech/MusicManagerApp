fd4f3eea0e0ca8938276310881804374cd8020c4
Conor Judge <conorjudgetech@icloud.com>
2024-03-16 20:56:01 +0000

    Stack

--- src/musicmanager/Stack.java
+++ src/musicmanager/Stack.java
@@ -7,5 +7,10 @@ package musicmanager;
  */
 
 public interface Stack <T> {//<T> the type of elements stored in the stack
-
+    //Adds an item to the top of the stack.
+    void push(T item);
+    //Removes and returns the item at the top of the stack.
+    T pop();
+    //Checks if the stack is empty
+    boolean isEmpty();
 }
